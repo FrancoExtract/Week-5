1d891965721ec55664037209a58f33bbd99c74c6
FrancoExtract <f.extra@student.han.nl>
2022-03-10 09:40:53 +0100

    ...

--- Weektaak 5 matplotlib.py
+++ Weektaak 5 matplotlib.py
@@ -4,22 +4,22 @@ import matplotlib as plt
 
 def graph_prepare(x, y):
     """
-    This function prepares a graph.
+    This function prepares a prototype graph.
 
     :param x: protein sequences on the x-axis
     :param y: match hits on the y-axis (in percentages)
     """
 
     plt.plot(x, y, 'g-')  # Plots out the axes and makes the line green
-    plt.title("Total counts of exons per chromosome")  # Title for the graph
-    plt.xlabel("Chromosome")  # Title for the x-axis
-    plt.ylabel("Exon count")  # Title for the y-axis
+    plt.title("")  # Title for the graph
+    plt.xlabel("")  # Title for the x-axis
+    plt.ylabel("")  # Title for the y-axis
     plt.show()  # Makes the graph show up when running the program
 
 
-def graph_plotter(gc_list, file_name, reading_distance):
-    # calculation = seq_len / reading_distance
-    # print(f"the result of {seq_len} / {reading_distance} =\n {calculation}")
+def graph_plotter(gc_list, file_name, reading_distance, seq_len):
+    calculation = seq_len / reading_distance
+    print(f"the result of {seq_len} / {reading_distance} =\n {calculation}")
     y = gc_list
     x = range(len(gc_list))
     plt.scatter(x, y, 5, c=gc_list, cmap="Reds")
@@ -43,7 +43,7 @@ def main():
     # Functions
     file_list = [file1, file2, file3, file4]
     graph_prepare(1, 1)
-    graph_plotter(..., file_list, 10000)
+    graph_plotter(..., file_list, 10000, 150000)
 
 
 main()
